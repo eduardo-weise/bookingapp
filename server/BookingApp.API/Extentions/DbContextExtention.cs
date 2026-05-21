1dce44e27346e4f47b47c97d909339dddc3e046b
Eduardo Weise <weise.eduardo@gmail.com>
2026-05-21 10:29:30 -0300

    chore: configuração de seed do usuário admin

--- server/BookingApp.API/Extentions/DbContextExtention.cs
+++ server/BookingApp.API/Extentions/DbContextExtention.cs
@@ -20,6 +20,24 @@ public static class DbContextExtension
 			using var scope = app.Services.CreateScope();
 			var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 			await dbContext.Database.MigrateAsync();
+
+			// Seed Admin User
+			var adminEmail = "weise.eduardo@gmail.com";
+			if (!await dbContext.Users.AnyAsync(u => u.Email == adminEmail))
+			{
+				var passwordHash = BookingApp.Infrastructure.Settings.Authentication.PasswordHasher.Hash("P@ssw0rd");
+				var adminUser = new BookingApp.Domain.Entities.User(
+					email: adminEmail,
+					passwordHash: passwordHash,
+					name: "Eduardo Wesie",
+					phoneNumber: "55981035906",
+					cpf: "01861126026",
+					role: "Admin"
+				);
+
+				await dbContext.Users.AddAsync(adminUser);
+				await dbContext.SaveChangesAsync();
+			}
 		}
 	}
 }
